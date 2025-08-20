1d112684847fda6ad69a6146de756733fec14130
Lloydchogarie <lloyddonnel44@gmail.com>
2025-08-20 11:21:36 +0200

    first commit

--- app/ServicesPage/page.tsx
+++ app/ServicesPage/page.tsx
@@ -5,6 +5,8 @@ import Link from 'next/link';
 import Image from 'next/image';
 import './Services.scss';
 import { FaShip, FaBox, FaTruck, FaPlane, FaWarehouse, FaBoxes, FaClipboardList } from 'react-icons/fa';
+import { FaFacebookF, FaInstagram, FaTwitter, FaWhatsapp } from 'react-icons/fa';
+
 
 export default function Services() {
   // First container (Sea, Air, Land)
@@ -245,14 +247,60 @@ export default function Services() {
                   </div>
                 </div>
               </section>
+
+              {/*=====6 FOOTER OUR FOOTER */}
+
+              <footer className="footerSection">
+                    <div className="footerInnerContainer">
+                      <div className="footerContainer">
+                        <div className="footerLeft">
+                            <img src="/shiplink-logo2.png" alt="ShipLink Logo" className="logoImage" />
+                            <p className="tagline">Fast, reliable, and smart logistics solutions</p>
+                          <div className="socialIcons">
+                            <FaFacebookF />
+                            <FaInstagram />
+                            <FaTwitter />
+                          </div>
+                        </div>
+              
+                        <div className="footerLinks">
+                          <div>
+                            <h3>Platforms</h3>
+                            <ul>
+                              <li>Web App</li>
+                              <li>Mobile App</li>
+                              <li>API Access</li>
+                            </ul>
+                          </div>
+                          <div>
+                            <h3>About</h3>
+                            <ul>
+                              <li>Our Story</li>
+                              <li>Careers</li>
+                              <li>Blog</li>
+                            </ul>
+                          </div>
+                        </div>
+              
+                        <div className="footerRight">
+                          <h3>Contact Us</h3>
+                          <button className="whatsappBtn">
+                            <FaWhatsapp />
+                            Get in Touch
+                          </button>
+                        </div>
+                      </div>
+              
+                      <div className="footerCopyright">
+                        <div className="footerLine"></div>
+                        <p>© {new Date().getFullYear()} CMLogistics. All rights reserved.</p>
+                      </div>
+                    </div>
+                  </footer>
       
 
        
-             
-
-
-
-
+      
     </>
   );
 }
