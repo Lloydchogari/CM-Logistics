8bdbdce8d9b53e6da60aff08d2ca919333e19a22
Lloydchogarie <lloyddonnel44@gmail.com>
2025-08-20 12:01:24 +0200

    first commit

--- app/Contact/page.tsx
+++ app/Contact/page.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import React, { useEffect } from 'react';
 import Image from 'next/image';
 import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from 'react-icons/fa'; // Font icons
+import { FaFacebookF, FaInstagram, FaTwitter, FaWhatsapp } from 'react-icons/fa';
 import Typed from 'typed.js'; // For typing effect
 import './Contact.scss';
 
@@ -146,6 +147,58 @@ export default function ContactUs() {
           </div>
         </div>
       </section>
+
+
+    {/*==FOOTER SECTION*/}
+
+     <footer className="footerSection">
+          <div className="footerInnerContainer">
+            <div className="footerContainer">
+              <div className="footerLeft">
+                  <img src="/shiplink-logo2.png" alt="ShipLink Logo" className="logoImage" />
+                  <p className="tagline">Fast, reliable, and smart logistics solutions</p>
+                <div className="socialIcons">
+                  <FaFacebookF />
+                  <FaInstagram />
+                  <FaTwitter />
+                </div>
+              </div>
+    
+              <div className="footerLinks">
+                <div>
+                  <h3>Platforms</h3>
+                  <ul>
+                    <li>Web App</li>
+                    <li>Mobile App</li>
+                    <li>API Access</li>
+                  </ul>
+                </div>
+                <div>
+                  <h3>About</h3>
+                  <ul>
+                    <li>Our Story</li>
+                    <li>Careers</li>
+                    <li>Blog</li>
+                  </ul>
+                </div>
+              </div>
+    
+              <div className="footerRight">
+                <h3>Contact Us</h3>
+                <button className="whatsappBtn">
+                  <FaWhatsapp />
+                  Get in Touch
+                </button>
+              </div>
+            </div>
+    
+            <div className="footerCopyright">
+              <div className="footerLine"></div>
+              <p>© {new Date().getFullYear()} CMLogistics. All rights reserved.</p>
+            </div>
+          </div>
+        </footer>
+
     </>
   );
 }
