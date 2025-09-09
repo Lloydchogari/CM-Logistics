e752003f75e1c6ef0d2a7b369c1ae8043c436976
Lloydchogarie <lloyddonnel44@gmail.com>
2025-09-09 10:20:12 +0200

    first commit

--- components/Offices.jsx
+++ components/Offices.jsx
@@ -16,25 +16,27 @@ const Offices = () => {
       <h3 className="countrySubheading">China</h3>
 
       <div className="officeWrapper">
-        <div className="officeLeft">
-          <div className="officeCardsRow">
-            <div className="officeCard">
-              <h3>China</h3>
-              <p><FaMapMarkerAlt className="icon" /> 88 NanJing Road, Shanghai</p>
-              <p><FaPhoneAlt className="icon" /> +86 21 1234 5678</p>
-              <p><FaEnvelope className="icon" /> china@shiplink.com</p>
-            </div>
-            <div className="officeCard">
-              <h3>Zimbabwe</h3>
-              <p><FaMapMarkerAlt className="icon" /> 45 Samora Machel Ave, Harare</p>
-              <p><FaPhoneAlt className="icon" /> +263 77 123 4567</p>
-              <p><FaEnvelope className="icon" /> zim@shiplink.com</p>
+        <div className="officeCardsRow mobileFlip">
+          <div className="flipCard">
+            <div className="flipCardInner">
+              {/* Front side */}
+              <div className="flipCardFront officeCard">
+                <h3>China</h3>
+                <p><FaMapMarkerAlt className="icon" /> 88 NanJing Road, Shanghai</p>
+                <p><FaPhoneAlt className="icon" /> +86 21 1234 5678</p>
+                <p><FaEnvelope className="icon" /> china@shiplink.com</p>
+              </div>
+              {/* Back side */}
+              <div className="flipCardBack officeCard">
+                <h3>Zimbabwe</h3>
+                <p><FaMapMarkerAlt className="icon" /> 45 Samora Machel Ave, Harare</p>
+                <p><FaPhoneAlt className="icon" /> +263 77 123 4567</p>
+                <p><FaEnvelope className="icon" /> zim@shiplink.com</p>
+              </div>
             </div>
           </div>
-          <p className="officeNote">
-            Clients from both Zimbabwe and China are welcome to visit us physically at our local offices for more information. We are open 24/7 to serve you better.
-          </p>
         </div>
+
 
           <div className="officeVideo">
             <video autoPlay loop muted playsInline>
